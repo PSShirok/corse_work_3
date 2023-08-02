ba566c75e932efb6800e0a150bb6268fcc8078e3
Павел <claid07@mail.ru>
2023-08-02 20:33:49 +0400

    delete check.py, create get_date_sort in utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import json
 
+
 def load_operations():
     """
     открываем файл с операциями
@@ -7,3 +8,15 @@ def load_operations():
     """
     with open("operations.json", "r", encoding="utf-8") as file:
         return json.load(file)
+
+
+card_operations = load_operations()
+
+
+def get_date_sort():
+    date_operations = []
+    for operation in card_operations:
+        if "date" in operation:
+            date_operations.append(operation['date'])
+        date_operations.sort()
+    return date_operations[-5:]
